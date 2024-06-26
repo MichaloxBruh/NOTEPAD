12dd7742eb1359faf6e98f3e123edf9c32f2e153
MichaloxBruh <79116858+MichaloxBruh@users.noreply.github.com>
2024-06-26 17:44:40 +0200

    Better Saving/Loading

--- NOTEPAD/Form1.Designer.cs
+++ NOTEPAD/Form1.Designer.cs
@@ -31,24 +31,23 @@
             richTextBox1 = new RichTextBox();
             button2 = new Button();
             button1 = new Button();
-            textBox1 = new TextBox();
-            label1 = new Label();
+            openFileDialog1 = new OpenFileDialog();
             SuspendLayout();
             // 
             // richTextBox1
             // 
-            richTextBox1.Location = new Point(-4, 28);
+            richTextBox1.Location = new Point(-4, 29);
             richTextBox1.Name = "richTextBox1";
-            richTextBox1.Size = new Size(805, 429);
+            richTextBox1.Size = new Size(1172, 635);
             richTextBox1.TabIndex = 1;
             richTextBox1.Text = "";
             richTextBox1.TextChanged += richTextBox1_TextChanged;
             // 
             // button2
             // 
-            button2.Location = new Point(98, -1);
+            button2.Location = new Point(135, -1);
             button2.Name = "button2";
-            button2.Size = new Size(109, 33);
+            button2.Size = new Size(144, 33);
             button2.TabIndex = 2;
             button2.Text = "Save";
             button2.UseVisualStyleBackColor = true;
@@ -58,36 +57,22 @@
             // 
             button1.Location = new Point(-4, -1);
             button1.Name = "button1";
-            button1.Size = new Size(106, 33);
+            button1.Size = new Size(143, 33);
             button1.TabIndex = 3;
             button1.Text = "Load";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click_1;
             // 
-            // textBox1
+            // openFileDialog1
             // 
-            textBox1.Location = new Point(257, 2);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(531, 23);
-            textBox1.TabIndex = 4;
-            textBox1.TextChanged += textBox1_TextChanged;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(226, 10);
-            label1.Name = "label1";
-            label1.Size = new Size(34, 15);
-            label1.TabIndex = 5;
-            label1.Text = "Path:";
+            openFileDialog1.FileName = "openFileDialog1";
+            openFileDialog1.FileOk += openFileDialog1_FileOk;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(label1);
-            Controls.Add(textBox1);
+            ClientSize = new Size(1169, 665);
             Controls.Add(button1);
             Controls.Add(button2);
             Controls.Add(richTextBox1);
@@ -95,7 +80,6 @@
             Text = "home-made notepad";
             Load += Form1_Load;
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -103,7 +87,6 @@
         public RichTextBox richTextBox1;
         private Button button2;
         private Button button1;
-        private TextBox textBox1;
-        private Label label1;
+        private OpenFileDialog openFileDialog1;
     }
 }
--- NOTEPAD/Form1.cs
+++ NOTEPAD/Form1.cs
@@ -6,10 +6,11 @@ namespace NOTEPAD
 {
     public partial class Form1 : Form
     {
-       bool didithappentimer = false;
+        bool didithappentimer = false;
         bool didithappentimer2 = false;
-       string filepath = "";
-       string SaveText = "";
+        string filepath = "";
+        string filepath2 = "";
+        string SaveText = "";
         public Form1()
         {
             InitializeComponent();
@@ -27,11 +28,12 @@ namespace NOTEPAD
         //Save
         public void button1_Click(object sender, EventArgs e)
         {
-            if ((filepath != null) && (filepath != "") && (didithappentimer == false))
-                {
-                    string SaveText = richTextBox1.Text;
+            //if ((filepath != null) && (filepath != "") && (didithappentimer == false))
+            if (didithappentimer == false)
+            {
+                string SaveText = richTextBox1.Text;
                 NP_Save();
-                };
+            };
         }
         //Load
         public void button1_Click_1(object sender, EventArgs e)
@@ -42,33 +44,63 @@ namespace NOTEPAD
             }
         }
 
-        public void textBox1_TextChanged(object sender, EventArgs e)
-        {
-            filepath = textBox1.Text;
-        }
-    
+        //public void textBox1_TextChanged(object sender, EventArgs e)
+        //{
+        //filepath = textBox1.Text;
+        //}
+
         public void NP_Save()
         {
             didithappentimer = true;
 
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog1.Title = "Save an Text File";
+            saveFileDialog1.ShowDialog();
+            {
+                filepath = Path.GetFullPath(saveFileDialog1.FileName);
+
+            }
+           
+            Thread.Sleep(1000);
+            
             FileStream fs = File.Create(filepath);
             fs.Close();
             string SaveText = richTextBox1.Text;
             File.WriteAllText(filepath, SaveText);
 
             Thread.Sleep(2000);
-            
+
             didithappentimer = false;
         }
 
         public void NP_Load()
         {
             didithappentimer2 = true;
-            string LoadText = File.ReadAllText(filepath);
+            
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //Get the path of specified file
+                filepath2 = openFileDialog.FileName;
+            }
+            
+            string LoadText = File.ReadAllText(filepath2);
             richTextBox1.AppendText(LoadText);
 
             Thread.Sleep(2000);
             didithappentimer2 = false;
+        }
+
+        private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
         }
     }
 }
